697f34faf95ec11b34dcab1f5b59626fb7e524bb
slicevision <slice.vision.official@gmail.com>
2025-06-14 12:34:32 +0200

    Update railway.nix

--- railway.nix
+++ railway.nix
@@ -1,5 +1,15 @@
-{ pkgs }: {
+{ pkgs }:
+
+let
+  pythonEnv = pkgs.python311.withPackages (ps: with ps; [
+    flask
+    flask_cors
+    requests
+    gunicorn
+  ]);
+in {
   deps = [
     pkgs.ffmpeg
+    pythonEnv
   ];
 }
